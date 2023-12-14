03ab64cae5f9e2d987e7b5dbf628b56c1f9e66d0
zephyrus <joelogira@gmail.com>
2023-12-14 21:15:29 +0300

    Created Contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,18 @@
-const Contact = () => {
+import Banner from './Banner'
+import Locations from './Locations'
+import Message from './Message'
+
+export const metadata = {
+    title: 'Sann Opicals Limited | Contact',
+    description: 'Contact us for any queries you may have about our optical shop and we will get back to you as soon as possible.'
+}
+
+export default function page() {
     return (
-        <main>
-            Contact
+        <main className='w-full'>
+            <Banner />
+            <Locations />
+            <Message />
         </main>
     )
 }
-
-export default Contact
