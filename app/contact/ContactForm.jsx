2626484e9938b2342d23293a66fdbd1d630c6ee5
zephyrus <joelogira@gmail.com>
2023-12-15 18:58:13 +0300

    Created contact form

---
+++ app/contact/ContactForm.jsx
@@ -0,0 +1,120 @@
+"use client"
+
+import { useForm } from 'react-hook-form'
+import { object, string } from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import ButtonSubmit from '../utils/ButtonSubmit'
+// import emailjs from '@emailjs/browser'
+
+const ContactForm = () => {
+    // const serviceId = 'service_l8884vh';
+    // const templateId = 'template_igrw7mg';
+    // const publicKey = 'nvaDyrzZ-1b--8sls'; 
+
+    const schema = object({
+        name: string().required('Please Enter Your Name'),
+        email: string().email('Enter a Valid Email').required('Enter a Valid Email'),
+        subject: string().required('Enter Subject'),
+        message: string().required('Write us a Message'),
+    });
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(schema),
+    });
+
+    const sendEmail = () => {
+        reset()
+    }
+
+    // const sendEmail = (data) => {
+    //     const { name, email, subject, message } = data;
+    //     const templateParams = {
+    //         from_name: name,
+    //         from_email: email,
+    //         to_name: 'Oyugis East SDA Church',
+    //         subject: subject,
+    //         message: message,
+    //     };
+
+    //     emailjs
+    //         .send(serviceId, templateId, templateParams, publicKey)
+    //         .then((response) => {
+    //             console.log('Email sent successfully', response);
+    //             reset();
+    //         })
+    //         .catch((error) => {
+    //             console.log('Error sending message', error);
+    //         });
+    // };
+
+    return (
+        <>
+            <h3 className="pb-6 capitalize text-2xl text-tertiary font-semibold lg:text-5xl lg:pb-8">
+                Send us a message
+            </h3>
+            <form onSubmit={handleSubmit(sendEmail)} className="flex flex-col space-y-6">
+                <div className="flex flex-col space-y-1">
+                    <input
+                        type="text"
+                        placeholder="Full Name"
+                        name="name"
+                        className="bg-[#F5F9F9] p-4 focus:outline-none focus:bg-transparent focus:border focus:border-[#F5F9F9] rounded"
+                        {...register('name')}
+                    />
+                    {errors.name && (
+                        <p className="text-red-500">{errors.name?.message}</p>
+                    )}
+                </div>
+
+                <div className="flex flex-col space-y-1">
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email Address"
+                        className="bg-[#F5F9F9] p-4 focus:outline-none focus:bg-transparent focus:border focus:border-[#F5F9F9] rounded"
+                        {...register('email')}
+                    />
+                    {errors.email && (
+                        <p className="text-red-500">{errors.email?.message}</p>
+                    )}
+                </div>
+
+                <div className="flex flex-col space-y-1">
+                    <input
+                        type="text"
+                        name="subject"
+                        placeholder="Subject"
+                        className="bg-[#F5F9F9] p-4 focus:outline-none focus:bg-transparent focus:border focus:border-[#F5F9F9] rounded"
+                        {...register('subject')}
+                    />
+                    {errors.subject && (
+                        <p className="text-red-500">{errors.subject?.message}</p>
+                    )}
+                </div>
+
+                <div className="flex flex-col space-y-1">
+                    <textarea
+                        cols="30"
+                        rows="6"
+                        name="message"
+                        placeholder="Message"
+                        className="bg-[#F5F9F9] p-4 focus:outline-none focus:bg-transparent focus:border focus:border-[#F5F9F9] rounded"
+                        {...register('message')}
+                    ></textarea>
+                    {errors.message && (
+                        <p className="text-red-500">{errors.message?.message}</p>
+                    )}
+                </div>
+                <ButtonSubmit type={'submit'} title={'Send Message'} />
+            </form>
+        </>
+    );
+};
+
+export default ContactForm;
+
