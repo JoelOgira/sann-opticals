0c4e37acda3ff1590d7bc574f9b127d9bf36e375
zephyrus <joelogira@gmail.com>
2023-12-15 18:56:39 +0300

    Updated content in the footer component

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -9,15 +9,15 @@ const Footer = () => {
     <footer className="top-full sticky">
       <div className="flex flex-col lg:flex-row text-white">
 
-        <div className="bg-primary px-4 py-10 lg:w-1/4 lg:py-20">
-          <div className="md:container md:mx-auto md:w-5/6">
+        <div className="bg-primary px-4 py-10 lg:px-12 lg:w-1/4 lg:py-20">
+          <div className="">
             <h4 className="text-2xl font-bold pb-6">
               Sann Opticals limited
             </h4>
 
             <div className="flex flex-col space-y-5">
               <p className="flex space-x-3">
-                <span  > <MdLocalPhone className='text-xl' /> </span>
+                <span> <MdLocalPhone className='text-xl' /> </span>
                 <span className="flex flex-col space-y-2">
                   <span>Mombasa: +254721520641</span>
                   <span>Kilifi: +254705473800</span>
@@ -44,8 +44,8 @@ const Footer = () => {
 
         </div>
 
-        <div className="bg-black [&>div]:px-4  py-10 lg:w-3/4 lg:py-20 lg:px-36">
-          <div className="md:container md:mx-auto md:w-5/6">
+        <div className="bg-black [&>div]:px-4  py-10 lg:w-3/4 lg:py-20 lg:px-12">
+          <div className="">
             <div className="grid grid-cols gap-y-10 pb-8 [&>div]:flex [&>div]:flex-col md:grid-cols-3 md:gap-y-0 lg:pb-12">
               <div className='flex flex-col space-y-3'>
                 <Link className='text-xl font-semibold pb-4' href={'/'}>Our company</Link>
@@ -69,7 +69,7 @@ const Footer = () => {
             </div>
           </div>
 
-          <hr className='border-[0.2px] w-full' />
+          <hr className=' border-gray-400 w-full' />
 
           <div className="top-full sticky pt-6 text-gray-400 text-center ">
             <p>Copyright &copy; Sann Opticals Limited {thisYear}</p>
