13294c026c2f6f1c02d41cca46107e6cb84c42ff
zephyrus <joelogira@gmail.com>
2023-12-18 19:42:32 +0300

    Created imports file

---
+++ app/components/imports.jsx
@@ -0,0 +1,17 @@
+import aon from '../../public/aon-minnet.webp'
+import britam from '../../public/britam.webp'
+import cic from '../../public/cic.webp'
+import makl from '../../public/makl.webp'
+import nhif from '../../public/nhif.webp'
+import trident from '../../public/Trident-Insurance.webp'
+
+export const acceptedInsurance = [
+    { id: 1, img: aon, alt: "aon-minnet" },
+    { id: 2, img: britam, alt: "britam" },
+    { id: 3, img: cic, alt: "cic" },
+    { id: 4, img: makl, alt: "makl" },
+    { id: 5, img: nhif, alt: "nhif" },
+    { id: 6, img: trident, alt: "trident" },
+]
+
+
