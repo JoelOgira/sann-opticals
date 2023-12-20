169eebefe28baabaedf1df1455db8fc83b0959ae
zephyrus <joelogira@gmail.com>
2023-12-20 22:03:29 +0300

    Changed button background color

--- app/utils/ButtonSubmit.jsx
+++ app/utils/ButtonSubmit.jsx
@@ -2,7 +2,7 @@
 const ButtonSubmit = ({ type, title }) => {
     return (
         <div className="flex justify-center md:items-start md:justify-start">
-            <button type={type} className='bg-secondary px-6 py-2 rounded text-white hover:text-tertiary hover:scale-105 hover:ease-in-out hover:duration-200'>
+            <button type={type} className='bg-brightRed px-6 py-2 rounded text-white hover:scale-105 hover:ease-in-out hover:duration-200'>
                 {title}
             </button>
         </div>
