5b40312cb40b34c49078335d865558c340a7e8a5
zephyrus <joelogira@gmail.com>
2023-12-20 22:00:04 +0300

    Added servicesSnippet for rendering

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Insurance from "./components/home/Insurance"
 import EmblaCarousel from "./components/home/EmblaCarousel"
+import ServicesSnippet from "./components/home/ServicesSnippet"
 
 export default function Home() {
   return (
     <main >
       <EmblaCarousel />
+      <ServicesSnippet />
       <Insurance />
     </main>
   )
