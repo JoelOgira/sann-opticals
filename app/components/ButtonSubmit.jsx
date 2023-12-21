9ddeac273a31583bcfed4510e99a3f12801abf5b
zephyrus <joelogira@gmail.com>
2023-12-21 11:15:36 +0300

    created submit button

---
+++ app/components/ButtonSubmit.jsx
@@ -0,0 +1,12 @@
+
+const ButtonSubmit = ({ type, title }) => {
+    return (
+        <div className="flex justify-center md:items-start md:justify-start">
+            <button type={type} className='bg-brightRed px-6 py-2 rounded text-white hover:scale-105 hover:ease-in-out hover:duration-200'>
+                {title}
+            </button>
+        </div>
+    )
+}
+
+export default ButtonSubmit
