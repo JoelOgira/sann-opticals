3adc218cea93961f650421de718327e0cde9a9f5
zephyrus <joelogira@gmail.com>
2023-12-26 19:44:05 +0300

    Updated the buttonSubmit component

--- app/components/ButtonSubmit.jsx
+++ app/components/ButtonSubmit.jsx
@@ -1,7 +1,7 @@
 
 const ButtonSubmit = ({ type, title }) => {
     return (
-        <div className="flex justify-center md:items-start md:justify-start">
+        <div className="flex justify-center font-semibold md:items-start md:justify-start">
             <button type={type} className='bg-brightRed px-6 py-2 rounded text-white hover:scale-105 hover:ease-in-out hover:duration-200'>
                 {title}
             </button>
