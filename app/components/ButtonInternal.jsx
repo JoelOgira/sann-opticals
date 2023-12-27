baca6079f8003f27a0afe65f356dc42c66b2fdef
zephyrus <joelogira@gmail.com>
2023-12-27 19:08:53 +0300

    Added props and changed the button styling

--- app/components/ButtonInternal.jsx
+++ app/components/ButtonInternal.jsx
@@ -1,11 +1,11 @@
 import Link from 'next/link'
 
-const ButtonInternal = () => {
+const ButtonInternal = ({ href, title }) => {
     return (
         <div className="flex justify-center md:items-start md:justify-start">
             <button
-                className='bg-secondary
-                px-6 py-2 rounded-full text-white hover:text-tertiary 
+                className='bg-brightRed
+                px-6 py-3 rounded font-semibold text-white hover:text-tertiary 
                 hover:scale-105 hover:ease-in-out hover:duration-200'
             >
                 <Link href={`${href}`}>
