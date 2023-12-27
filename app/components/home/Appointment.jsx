269c113cd51732b542d74e3d873e2c8d286326f8
zephyrus <joelogira@gmail.com>
2023-12-27 19:12:32 +0300

    Added appointment form to the component

--- app/components/home/Appointment.jsx
+++ app/components/home/Appointment.jsx
@@ -1,15 +1,16 @@
+import AppointmentForm from "./AppointmentForm"
 import Faq from "./Faq"
 
 const Appointment = () => {
     return (
-        <section className='w-full bg-white'>
-            <div className="flex flex-col-reverse px-4 md:flex-row lg:px-12">
-                <div className=" grid items-center md:w-1/2 ">
+        <section className='w-full bg-white py-24'>
+            <div className="flex flex-col space-y-12 px-4 md:flex-row md:space-y-0 lg:px-72">
+                <div className=" grid items-center md:w-1/2 md:pr-12 ">
                     <Faq />
                 </div>
 
                 <div className=" grid items-center md:w-1/2 ">
-
+                    <AppointmentForm />
                 </div>
             </div>
         </section>
