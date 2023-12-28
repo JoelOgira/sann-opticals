1001f7eaf4c232df76a96194cd1ce52f673cb72b
zephyrus <joelogira@gmail.com>
2023-12-28 20:38:31 +0300

    Updated styling in the single service

--- app/components/home/SingleService.jsx
+++ app/components/home/SingleService.jsx
@@ -1,10 +1,10 @@
 
 const SingleService = ({ icon, title, description }) => {
     return (
-        <div className="bg-white rounded px-5 flex flex-col space-y-3 items-center justify-center text-justify py-16 border md:py-28">
-            <h3 className="text-lightBlue text-2xl p-2 rounded-full bg-brightRed">{icon}</h3>
+        <div className="bg-white rounded px-10 flex flex-col space-y-6 items-center justify-center text-center py-10 shadow-md md:py-20">
+            <h3 className="text-lightBlue text-4xl p-2 rounded-full bg-brightRed">{icon}</h3>
             <h4 className='text-lg font-semibold'>{title}</h4>
-            <p>{description}</p>
+            <p className="text-gray-800">{description}</p>
         </div>
     )
 }
