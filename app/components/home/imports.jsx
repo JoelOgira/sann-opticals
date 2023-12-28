fee9e1f978cec55440ac67680e5a1105cc89911d
zephyrus <joelogira@gmail.com>
2023-12-28 20:37:09 +0300

    Added new images for import

--- app/components/home/imports.jsx
+++ app/components/home/imports.jsx
@@ -4,6 +4,37 @@ import cic from '../../../public/cic.webp'
 import makl from '../../../public/makl.webp'
 import nhif from '../../../public/nhif.webp'
 import trident from '../../../public/Trident-Insurance.webp'
+import armani from '../../../public/armani1.webp'
+import armaniSide from '../../../public/armani-side.webp'
+import burberry from '../../../public/burberry.webp'
+import burberrySide from '../../../public/burberry-side.webp'
+import prada from '../../../public/prada1.webp'
+import pradaSide from '../../../public/prada1-side.webp'
+import rayban1 from '../../../public/rayban1.webp'
+import rayban1Side from '../../../public/rayban1-side.webp'
+import rayban2 from '../../../public/rayban2.webp'
+import rayban2Side from '../../../public/rayban2-side.webp'
+import swarovski from '../../../public/swarovski.webp'
+import swarovskiSide from '../../../public/swavroski1-side.webp'
+import versace from '../../../public/versace.webp'
+import versaceSide from '../../../public/versace-side.webp'
+import raybanSun from '../../../public/raybanSun.webp'
+import raybanSunSide from '../../../public/raybanSun-side.webp'
+import versaceSun from '../../../public/versaceSun.webp'
+import versaceSunSide from '../../../public/versaceSun-side.webp'
+import pradaSun from '../../../public/pradaSun.webp'
+import pradaSunSide from '../../../public/pradaSun-side.webp'
+import swarovskiSun from '../../../public/swavroskiSun.webp'
+import swarovskiSunSide from '../../../public/swavroskiSun-side.webp'
+import toryburch from '../../../public/toryburch.webp'
+import toryburchSide from '../../../public/toryburch-side.webp'
+import oakleySun from '../../../public/oakleySun.webp'
+import oakleySunSide from '../../../public/oakleySun-side.webp'
+import burberrySun from '../../../public/burberrySun.webp'
+import burberrySunSide from '../../../public/burberrySun-side.webp'
+import versaceSun2 from '../../../public/versaceSun2.webp'
+import versaceSun2Side from '../../../public/versaceSun2-side.webp'
+
 
 export const acceptedInsurance = [
     { id: 1, img: aon, alt: "aon-minnet" },
@@ -14,4 +45,24 @@ export const acceptedInsurance = [
     { id: 6, img: trident, alt: "trident" },
 ]
 
+export const glassCatalog = [
+    { id: 1, img: [ rayban1, rayban1Side ], title: "Ray-Ban" },
+    { id: 2, img: [ prada, pradaSide ], title: "Prada" },
+    { id: 3, img: [ swarovski, swarovskiSide ], title: "Swarovski" },
+    { id: 4, img: [ rayban2, rayban2Side ], title: "Ray-Ban" },
+    { id: 5, img: [ armani, armaniSide ], title: "Giorgio Armani" },
+    { id: 6, img: [ versace, versaceSide ], title: "Versace" },
+    { id: 7, img: [ burberry, burberrySide ], title: "Burberry" }
+]
+
+export const sunglassCatalog = [
+    { id: 1, img: [ raybanSun, raybanSunSide ], title: "Ray-Ban" },
+    { id: 2, img: [ versaceSun, versaceSunSide ], title: "Versace" },
+    { id: 3, img: [ pradaSun, pradaSunSide ], title: "Prada" },
+    { id: 4, img: [ swarovskiSun, swarovskiSunSide ], title: "Swarovski" },
+    { id: 5, img: [ toryburch, toryburchSide ], title: "Tory Burch" },
+    { id: 6, img: [ oakleySun, oakleySunSide ], title: "Oakley" },
+    { id: 7, img: [ burberrySun, burberrySunSide ], title: "Burberry" },
+    { id: 8, img: [ versaceSun2, versaceSun2Side ], title: "Versace" }
+]
 
