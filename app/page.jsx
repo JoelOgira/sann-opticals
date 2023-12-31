7bb03a84846afa3b8d935a641d21c0646f9f6b60
zephyrus <joelogira@gmail.com>
2023-12-31 20:18:06 +0300

    Added new components for rendering

--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,18 @@ import Insurance from "./components/home/Insurance"
 import EmblaCarousel from "./components/home/EmblaCarousel"
 import ServicesSnippet from "./components/home/ServicesSnippet"
 import Appointment from "./components/home/Appointment"
+import Catalog from "./components/home/Catalog"
+import Showcase from "./components/home/Showcase"
 
 export default function Home() {
   return (
     <main >
       <EmblaCarousel />
+      <Catalog />
       <ServicesSnippet />
-      <Insurance />
+      <Showcase />
       <Appointment />
+      <Insurance />
     </main>
   )
 }
