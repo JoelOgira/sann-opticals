d6e661bbcd560501a2fc16a65cebd71ad8c9d144
zephyrus <joelogira@gmail.com>
2024-01-03 21:54:58 +0300

    created eyeExam component

---
+++ app/services/EyeExam.jsx
@@ -0,0 +1,118 @@
+/* eslint-disable react/no-unescaped-entities */
+
+
+const EyeExam = () => {
+    return (
+        <div className="bg-white px-4 py-24 lg:px-12">
+            <div className="flex flex-col space-y-8 py-8 lg:flex-row lg:space-y-0 lg:space-x-5">
+                <div className="lg:w-1/2">
+                    <p className="uppercase text-2xl md:text-4xl font-serif lg:text-6xl " >How often should<br /> you get an eye <br /> exam?</p>
+                </div>
+                <div className="lg:w-1/2">
+                    <div className="flex flex-col space-y-5">
+                        <p className="leading-6">
+                            It's never too early to schedule an eye exam. According to the Kenya Association of Opticians, eye exams are recommended for children at 6 months old, 3 years old and before starting first grade (age 6).
+                        </p>
+                        <p className="leading-6">
+                            Starting eye exams at an early age can help ensure a Kid’s eye care is developing normally before they begin school. And the benefits of eye exams are often reflected in their academic success.
+                        </p>
+                        <p className="leading-6">
+                            After age 6, eye exams are recommended every year for children who need glasses or contacts. If no vision problems are detected, a child should have a comprehensive eye exam every other year.
+                        </p>
+                        <p className="leading-6">
+                            The risk of eye disease increases after age 40. This makes annual eye exams even more important for older patients. Early detection is key to preventing and treating eye health problems. In fact, 80% of global visual impairment is avoidable if prevented or corrected with proper treatment.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <hr className="border my-10" />
+
+            <div className="flex flex-col space-y-8 py-8 lg:flex-row lg:space-y-0 lg:space-x-5">
+                <div className="lg:w-1/2">
+                    <p className="uppercase text-2xl md:text-4xl font-serif lg:text-6xl " >What happens  <br />during an eye <br /> exam?</p>
+                </div>
+                <div className="lg:w-1/2">
+                    <div className="flex flex-col space-y-5">
+                        <p className="leading-6">
+                            A typical eye exam checks your vision and the health of the eye itself. The Snellen eye chart is the first and most basic way to measure your visual acuity, which is how clearly you can see.
+                        </p>
+                        <p className="leading-6">
+                            If your visual acuity is not 20/20 (what’s considered “good” or “average” vision), your eye doctor will test for Common eye conditions. Common refractive errors include myopia (nearsightedness), hyperopia (farsightedness) and astigmatism.
+                        </p>
+                        <p className="leading-6">
+                            Your eye doctor will also conduct a series of tests designed to catch certain eye conditions and diseases. Sann Opticals offers the digital eye exam that helps eye doctors pinpoint eye changes over time.
+                        </p>
+                        <p className="leading-6">
+                            Specific eye exams can be done for people who have a pre-existing condition, such as diabetes. A diabetic eye exam pays special attention to the health of the retina and other parts of the eye that diabetes can affect. People with diabetes are 60% more likely to develop cataracts, which makes regular diabetic eye exams even more critical.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <hr className="border my-10" />
+
+            <div className="flex flex-col space-y-8 py-8 lg:flex-row lg:space-y-0 lg:space-x-5">
+                <div className="lg:w-1/2">
+                    <div className="flex flex-col space-y-5 lg:[&>p]:max-w-3xl">
+                        <p className="uppercase text-2xl md:text-4xl font-serif lg:text-6xl " >What can an eye<br />exam detect?</p>
+                        <p className="leading-6">
+                            The eye is one of the few places in the body where blood vessels are clearly visible. Because of this, eye doctors can check for common diseases that often appear in the eye first, such as diabetes and high blood pressure.
+                        </p>
+                        <p className="leading-6">
+                            These diseases usually develop later in life along with glaucoma and cataracts. However, other conditions like amblyopia (lazy eye) may develop during infancy.
+                        </p>
+                        <p className="leading-6">
+                            Beyond diabetes and high blood pressure, there are other conditions an eye exam can detect, such as thyroid eye disease and malignant eye melanoma.
+                        </p>
+                        <p className="leading-6">
+                            Many eye and vision problems have no obvious symptoms, which makes them harder to notice when they first develop. Preventative eye exams make early detection of these problems possible. Early detection allows for prompt care, which often makes treatment easier and more effective.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="h-[407px] md:h-[506px] lg:w-1/2 lg:h-[700px]">
+                    <div className="eyeExam-bg h-[407px] md:h-[506px] lg:h-[700px]">
+
+                    </div>
+                </div>
+            </div>
+
+            <hr className="border my-10" />
+
+            <div className="flex flex-col-reverse space-y-8 py-8 lg:flex-row lg:space-y-0 lg:space-x-5">
+                <div className="h-[407px] md:h-[506px] lg:w-1/2 lg:h-[700px]">
+                    <div className="eyeExam-bg2 h-[407px] md:h-[506px] lg:h-[700px]">
+
+                    </div>
+                </div>
+
+                <div className="pb-8 lg:pl-20 lg:pb-0 lg:w-1/2">
+                    <div className="flex flex-col space-y-5 lg:[&>p]:max-w-3xl">
+                        <p className="uppercase text-2xl md:text-4xl font-serif lg:text-6xl " > Computerized eye exam </p>
+                        <p className="leading-6">
+                            New precision technology has improved eye exams by giving you and your doctor a more complete picture of your eye health. For many eye doctors, advanced diagnostics have replaced older eye test methods.
+                        </p>
+                        <p className="leading-6">
+                            Here is some of the innovative technology you can look forward to at your next Sann Opticals eye exam:
+                        </p>
+                        <p className="leading-6">
+                            <span className="font-semibold">VISION PROFILER.</span>    This tool provides a detailed visual profile that’s like a fingerprint for your eye. It can even detect the differences in how your eye sees in daylight versus at nighttime.
+                        </p>
+                        <p className="leading-6">
+                            <span className="font-semibold">LENS PROFILER.</span>This digital lens meter measures your current prescription instantly and accurately. Then it calculates a prescription starting point so there’s no guesswork. It gives your eye doctor a quick baseline before starting your eye exam and can simulate a comparison of your vision before and after correction.
+                        </p>
+                        <p className="leading-6">
+                            <span className="font-semibold">DIGITAL RETINAL SCAN.</span>This digital retinal technology scans your eyes then prints out a 'map' of your interior eye system. This map gives your doctor a clear, comprehensive look at your eye health. Dilation often isn't needed with this technology, so you can leave afterward without having to worry about light sensitivity or difficulty focusing.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <hr className="border my-10" />
+
+        </div>
+    )
+}
+
+export default EyeExam
