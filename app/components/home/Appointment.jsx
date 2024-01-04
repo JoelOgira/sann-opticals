a30f11622a74769cfea4ab7bf363fa04f0e8130a
zephyrus <joelogira@gmail.com>
2024-01-04 20:15:37 +0300

    Adjusted the styling

--- app/components/home/Appointment.jsx
+++ app/components/home/Appointment.jsx
@@ -4,12 +4,12 @@ import Faq from "./Faq"
 const Appointment = () => {
     return (
         <section className='w-full bg-white py-24'>
-            <div className="flex flex-col space-y-12 px-4 md:flex-row md:space-y-0 xl:px-72">
-                <div className=" grid items-center md:w-1/2 md:pr-12 ">
+            <div className="flex flex-col space-y-12 px-4 lg:flex-row lg:space-y-0 xl:px-72">
+                <div className=" grid items-center lg:w-1/2 lg:pr-12 ">
                     <Faq />
                 </div>
 
-                <div className=" grid items-center md:w-1/2 ">
+                <div className=" grid items-center lg:w-1/2 ">
                     <AppointmentForm />
                 </div>
             </div>
