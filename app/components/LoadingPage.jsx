caf537ac594ae89d28a4ab2e59560854f3371e30
zephyrus <joelogira@gmail.com>
2024-01-08 13:11:54 +0300

    Created reusable loading component

---
+++ app/components/LoadingPage.jsx
@@ -0,0 +1,11 @@
+const LoadingPage = () => {
+    return (
+        <main className="min-h-screen grid justify-center items-center">
+            <h1 className="text-4xl font-bold">
+                Loading...
+            </h1>
+        </main>
+    )
+}
+
+export default LoadingPage
