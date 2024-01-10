f5b534a200a9ec64a8d6cf5a7da5d9982264a85f
zephyrus <joelogira@gmail.com>
2024-01-10 17:14:28 +0300

    Added a loading spinner to the component

--- app/components/LoadingPage.jsx
+++ app/components/LoadingPage.jsx
@@ -1,9 +1,16 @@
+"use client"
+
+import CircleLoader from 'react-spinners/CircleLoader'
+
 const LoadingPage = () => {
+
     return (
         <main className="min-h-screen grid justify-center items-center">
-            <h1 className="text-4xl font-bold">
-                Loading...
-            </h1>
+            <CircleLoader
+                size={150}
+                color={'#ec4949'}
+                loading={true}
+            />
         </main>
     )
 }
