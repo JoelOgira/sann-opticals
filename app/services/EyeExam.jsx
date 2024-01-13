71d57c67230e3c9ac9b2d92523101bb87a0a7dc3
zephyrus <joelogira@gmail.com>
2024-01-13 22:21:53 +0300

    Modified the styling

--- app/services/EyeExam.jsx
+++ app/services/EyeExam.jsx
@@ -5,7 +5,7 @@ const EyeExam = () => {
         <div className="bg-white px-4 py-16 md:py-20 lg:px-12">
             <div className="flex flex-col space-y-8 py-8 lg:flex-row lg:space-y-0 lg:space-x-5">
                 <div className="lg:w-1/2">
-                    <h2 className="uppercase text-lightRed text-2xl md:text-4xl  lg:text-6xl " >How often should<br /> you get an eye <br /> exam?</h2>
+                    <h2 className="uppercase text-lightRed text-2xl md:text-4xl  lg:text-5xl " >How often should<br /> you get an eye <br /> exam?</h2>
                 </div>
                 <div className="lg:w-1/2">
                     <div className="flex flex-col space-y-5">
@@ -29,7 +29,7 @@ const EyeExam = () => {
 
             <div className="flex flex-col space-y-8 py-8 lg:flex-row lg:space-y-0 lg:space-x-5">
                 <div className="lg:w-1/2">
-                    <h2 className="uppercase text-lightRed text-2xl md:text-4xl lg:text-6xl " >What happens  <br />during an eye <br /> exam?</h2>
+                    <h2 className="uppercase text-lightRed text-2xl md:text-4xl lg:text-5xl " >What happens  <br />during an eye <br /> exam?</h2>
                 </div>
                 <div className="lg:w-1/2">
                     <div className="flex flex-col space-y-5">
@@ -54,7 +54,7 @@ const EyeExam = () => {
             <div className="flex flex-col space-y-8 py-8 lg:flex-row lg:space-y-0 lg:space-x-5">
                 <div className="lg:w-1/2">
                     <div className="flex flex-col space-y-5 lg:[&>p]:max-w-3xl">
-                        <h2 className="uppercase text-lightRed text-2xl md:text-4xl lg:text-6xl " >What can an eye<br />exam detect?</h2>
+                        <h2 className="uppercase text-lightRed text-2xl md:text-4xl lg:text-5xl " >What can an eye<br />exam detect?</h2>
                         <p className="leading-6">
                             The eye is one of the few places in the body where blood vessels are clearly visible. Because of this, eye doctors can check for common diseases that often appear in the eye first, such as diabetes and high blood pressure.
                         </p>
@@ -88,7 +88,7 @@ const EyeExam = () => {
 
                 <div className="pb-8 lg:pl-20 lg:pb-0 lg:w-1/2">
                     <div className="flex flex-col space-y-5 lg:[&>p]:max-w-3xl">
-                        <h2 className="uppercase text-lightRed text-2xl md:text-4xl  lg:text-6xl " > Computerized eye exam </h2>
+                        <h2 className="uppercase text-lightRed text-2xl md:text-4xl  lg:text-5xl " > Computerized eye exam </h2>
                         <p className="leading-6">
                             New precision technology has improved eye exams by giving you and your doctor a more complete picture of your eye health. For many eye doctors, advanced diagnostics have replaced older eye test methods.
                         </p>
