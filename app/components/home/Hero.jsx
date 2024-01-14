6a750723715ee4983c76dd9b24956d997d4aa187
zephyrus <joelogira@gmail.com>
2024-01-14 21:18:10 +0300

    Created Hero Component

---
+++ app/components/home/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+    return (
+        <main className="w-full bg-gradient-to-tl from-blue-200 via-blue-100 to-blue-200 py-20 px-4 md:px-8 md:py-28 xl:px-72">
+
+        </main>
+    )
+}
+
+export default Hero
