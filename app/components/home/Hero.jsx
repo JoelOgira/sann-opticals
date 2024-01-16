46ec012baadc1d75db9d176ded45644d68448a13
zephyrus <joelogira@gmail.com>
2024-01-16 22:31:56 +0300

    Updated the component

--- app/components/home/Hero.jsx
+++ app/components/home/Hero.jsx
@@ -1,7 +1,10 @@
 const Hero = () => {
     return (
         <main className="w-full bg-gradient-to-tl from-blue-200 via-blue-100 to-blue-200 py-20 px-4 md:px-8 md:py-28 xl:px-72">
-
+            <div className="flex flex-col lg:flex-row">
+                <div className="lg:w-1/2"></div>
+                <div className="lg:w-1/2"></div>
+            </div>
         </main>
     )
 }
