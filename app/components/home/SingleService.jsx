3cfbfbafc829e66755c3cfe9ad9573a667340c46
zephyrus <joelogira@gmail.com>
2024-01-17 20:52:33 +0300

    Modified the layout in single service component

--- app/components/home/SingleService.jsx
+++ app/components/home/SingleService.jsx
@@ -1,10 +1,10 @@
 
 const SingleService = ({ icon, title, description }) => {
     return (
-        <div className="bg-white rounded px-10 flex flex-col space-y-6 items-center justify-center text-center py-10 shadow-md md:py-20">
-            <h3 className="text-lightBlue text-4xl p-2 rounded-full bg-brightRed">{icon}</h3>
+        <div className="bg-white rounded px-10 flex flex-col space-y-6 items-center justify-center text-center py-10 shadow-md md:py-20 md:px-10 md:justify-start md:items-start md:text-start">
+            <h3 className="text-lightBlue text-6xl p-2 rounded-full bg-brightRed">{icon}</h3>
             <h4 className='text-lg font-semibold'>{title}</h4>
-            <p className="text-gray-800">{description}</p>
+            <p className="text-gray-800 leading-8">{description}</p>
         </div>
     )
 }
