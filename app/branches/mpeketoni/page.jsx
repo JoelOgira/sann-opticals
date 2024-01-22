83443677f92d759915c5eb5dacc3006fa0687353
zephyrus <joelogira@gmail.com>
2024-01-22 21:02:48 +0300

    Created Mpeketoni branch page

---
+++ app/branches/mpeketoni/page.jsx
@@ -0,0 +1,11 @@
+import Banner from "../Banner"
+
+const Mpeketoni = () => {
+    return (
+        <main>
+            <Banner location={'Mpeketoni'} />
+        </main>
+    )
+}
+
+export default Mpeketoni
