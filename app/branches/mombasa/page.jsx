d9e92563507887a1875c48a063c65557d7bde885
zephyrus <joelogira@gmail.com>
2024-01-22 21:02:29 +0300

    Created Mombasa branch page

---
+++ app/branches/mombasa/page.jsx
@@ -0,0 +1,11 @@
+import Banner from "../Banner"
+
+const Mombasa = () => {
+    return (
+        <main>
+            <Banner location={'Mombasa'} />
+        </main>
+    )
+}
+
+export default Mombasa
