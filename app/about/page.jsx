88ae3c6b476c915f36a401897da5bdb523d3d169
zephyrus <joelogira@gmail.com>
2024-01-24 23:29:50 +0300

    Created about page

---
+++ app/about/page.jsx
@@ -0,0 +1,16 @@
+import Banner from "./Banner"
+
+export const metadata = {
+  title: 'Babs Optical | About',
+  description: 'Find more about our values as a company committed to providing excellent services to you.'
+}
+
+const About = () => {
+  return (
+    <main>
+      <Banner location={'About'} />
+    </main>
+  )
+}
+
+export default About
