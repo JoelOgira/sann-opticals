ce26150ba868583d08449166b04281231a14c876
zephyrus <joelogira@gmail.com>
2024-04-04 00:00:50 +0300

    Updated the Banner component

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,14 +1,14 @@
 import Banner from "./Banner"
 
 export const metadata = {
-  title: 'Babs Optical | About',
+  title: 'Sann Opticals Limited | About',
   description: 'Find more about our values as a company committed to providing excellent services to you.'
 }
 
 const About = () => {
   return (
     <main>
-      <Banner location={'About'} />
+      <Banner />
     </main>
   )
 }
